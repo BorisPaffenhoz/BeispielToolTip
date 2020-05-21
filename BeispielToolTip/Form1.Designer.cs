17625c62774b54c14091dcdd03e85dc3f3c2a9b8
Boirs Paffenholz <boris.paffen@web.de>
2020-05-21 07:35:54 +0200

    Projektdateien hinzufügen.

---
+++ BeispielToolTip/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BeispielToolTip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.tipButtons = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 117);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.toolTip2.SetToolTip(this.button1, "Button");
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // toolTip2
+            // 
+            this.toolTip2.BackColor = System.Drawing.Color.White;
+            this.toolTip2.ForeColor = System.Drawing.Color.Black;
+            this.toolTip2.OwnerDraw = true;
+            this.toolTip2.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip2.Draw += new System.Windows.Forms.DrawToolTipEventHandler(this.toolTip2_Draw);
+            this.toolTip2.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip2_Popup);
+            // 
+            // tipButtons
+            // 
+            this.tipButtons.Location = new System.Drawing.Point(25, 96);
+            this.tipButtons.Name = "tipButtons";
+            this.tipButtons.Size = new System.Drawing.Size(75, 23);
+            this.tipButtons.TabIndex = 3;
+            this.tipButtons.Text = "button2";
+            this.tipButtons.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(350, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(438, 376);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tipButtons);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolTip1.SetToolTip(this, "Fenster");
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button tipButtons;
+    }
+}
+
---
+++ BeispielToolTip/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeispielToolTip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            pictureBox1.Image = Properties.Resources.Hund;
+            toolTip2.SetToolTip(button1, "Hallo das ist ein Hund mit dem Namen Boris.\nDer ist so müde.");
+
+            
+
+        }
+
+        private new const int Margin = 10;
+
+       
+
+        private void toolTip2_Popup(object sender, PopupEventArgs e)
+        {
+            int image_wid = 2 * Margin +
+            Properties.Resources.Hund.Width;
+            int image_hgt = 2 * Margin +
+            Properties.Resources.Hund.Height;
+
+            
+
+            int wid = e.ToolTipSize.Width + 2 * Margin + image_wid;
+            int hgt = e.ToolTipSize.Height;
+            if (hgt < image_hgt) hgt = image_hgt;
+
+            e.ToolTipSize = new Size(wid, hgt);
+        }
+
+        
+
+        private void toolTip2_Draw(object sender, DrawToolTipEventArgs e)
+        {
+            // Draw the background and border.
+            e.DrawBackground();
+            e.DrawBorder();
+
+            // Draw the image.
+            e.Graphics.DrawImage(Properties.Resources.Hund,
+                Margin, Margin);
+
+            // Draw the text.
+            using (StringFormat sf = new StringFormat())
+            {
+                sf.Alignment = StringAlignment.Near;
+                sf.LineAlignment = StringAlignment.Near;
+
+                int image_wid = 2 * Margin +
+                    Properties.Resources.Hund.Width;
+
+                Rectangle rect = new Rectangle(image_wid, 0,
+                    e.Bounds.Width - image_wid, e.Bounds.Height);
+                e.Graphics.DrawString(
+                    e.ToolTipText, e.Font, Brushes.Aqua, rect, sf);
+                
+            }
+        }
+    }
+}
+        
+
+        
